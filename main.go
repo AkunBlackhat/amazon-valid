9d4aaa483e3241b6adda57fc4e0328c850cfa65f
Nama Saya <wearefakboy@gmail.com>
2019-11-29 16:48:45 +0700

    a

--- main.go
+++ main.go
@@ -17,8 +17,20 @@ import (
 const url = "https://www.amazon.com/ap/register%3Fopenid.assoc_handle%3Dsmallparts_amazon%26openid.identity%3Dhttp%253A%252F%252Fspecs.openid.net%252Fauth%252F2.0%252Fidentifier_select%26openid.ns%3Dhttp%253A%252F%252Fspecs.openid.net%252Fauth%252F2.0%26openid.claimed_id%3Dhttp%253A%252F%252Fspecs.openid.net%252Fauth%252F2.0%252Fidentifier_select%26openid.return_to%3Dhttps%253A%252F%252Fwww.smallparts.com%252Fsignin%26marketPlaceId%3DA2YBZOQLHY23UT%26clientContext%3D187-1331220-8510307%26pageId%3Dauthportal_register%26openid.mode%3Dcheckid_setup%26siteState%3DfinalReturnToUrl%253Dhttps%25253A%25252F%25252Fwww.smallparts.com%25252Fcontactus%25252F187-1331220-8510307%25253FappAction%25253DContactUsLanding%252526pf_rd_m%25253DA2LPUKX2E7NPQV%252526appActionToken%25253DlptkeUQfbhoOU3v4ShyMQLid53Yj3D%252526ie%25253DUTF8%252Cregist%253Dtrue"
 
 var ubidMain = ""
+var lines = 1
 
 func main() {
+	sad := `   ___                                         _    __      ___     __
+   /   |  ____ ___  ____ _____  ____  ____     | |  / /___ _/ (_)___/ /`
+
+	fmt.Println(sad)
+	fmt.Println("@AkunBlackhat")
+	fmt.Println("")
+
+	line := 1
+
+	totalData := getTotalData()
+
 	file, err := os.Open("./email.txt")
 
 	if err != nil {
@@ -33,18 +45,24 @@ func main() {
 	// I think we need a wait group, not sure.
 	wg := new(sync.WaitGroup)
 
-	for w := 1; w <= 10; w++ {
-		wg.Add(10)
-		go run(jobs, wg, results)
-	}
-
 	go func() {
 		scanner := bufio.NewScanner(file)
 		for scanner.Scan() {
+
 			jobs <- scanner.Text()
+			lines = line
+			line++
 		}
 		close(jobs)
+		color.New(color.FgGreen, color.BgBlack).Println("Done.")
+		os.Exit(2)
 	}()
+
+	for w := 1; w <= 20; w++ {
+		wg.Add(20)
+		// fmt.Println(lines)
+		go run(jobs, wg, results, totalData)
+	}
 
 	go func() {
 		wg.Wait()
@@ -56,12 +74,34 @@ func main() {
 	for v := range results {
 		counts += v
 	}
+	fmt.Print(counts)
 
 }
 
-func run(email <-chan string, wg *sync.WaitGroup, results <-chan int) {
+func getTotalData() int {
+	file, err := os.Open("./email.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+	counter := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		// line := scanner.Text()
+		counter++
+		// do something with your line
+	}
+
+	return counter
+}
+
+func run(email <-chan string, wg *sync.WaitGroup, results <-chan int, totalData int) {
 	defer wg.Done()
+	// line := 1
 	for j := range email {
+		// index := getIndex(totalData)
 		appActionToken, prevRID, siteState, workflowState := getHTMLComponent()
 		newFormData := generateFormData(appActionToken, prevRID, siteState, workflowState, j)
 		getCookie()
@@ -71,10 +111,20 @@ func run(email <-chan string, wg *sync.WaitGroup, results <-chan int) {
 			color.New(color.FgGreen, color.BgBlack).Println("LIVE : " + j)
 			createFile(j, "but an account already exists with the e-mail")
 		} else {
-			color.New(color.FgRed, color.BgBlack).Println("NOT LIVE : " + j)
+			color.New(color.FgRed, color.BgBlack).Println("DIE : " + j)
 			createFile(j, "")
 		}
+		// line++
 	}
+}
+
+func getIndex(totalData int) int {
+	var line int
+	for index := 0; index < totalData; index++ {
+		line = index + 1
+	}
+
+	return line
 }
 
 func createFile(email string, indicator string) {
@@ -91,7 +141,7 @@ func createFile(email string, indicator string) {
 			log.Fatal(err)
 		}
 	} else {
-		f, err := os.OpenFile("NOTLIVE.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		f, err := os.OpenFile("DIE.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -102,8 +152,6 @@ func createFile(email string, indicator string) {
 			log.Fatal(err)
 		}
 	}
-
-	//this is my change
 
 }
 
